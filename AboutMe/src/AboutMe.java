def405eac384c5fc03921b5ee1680da4e5a4efeb
jquaratiello2019 <jquaratiello2019@cchsraider.net>
2017-09-22 10:07:52 -0400

    initial commit

---
+++ AboutMe/src/AboutMe.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner (System.in);
+		String name;
+		String color;
+		
+		System.out.println("What is your name?");
+		name = reader.nextLine();
+		
+		System.out.println("What is your favorite color?");
+		color = reader.nextLine();
+		
+		reader.close();
+		
+		System.out.println("Hi " + name + ", " + color + " is a great color!" );
+		
+	}
+
+}
